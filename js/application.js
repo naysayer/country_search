fb223ef58e3d09868ebbf0a712f1fcc1a375b76a
Codemis <johnathan@still-water.com>
2012-04-26 20:22:01 -0700

    Added a message for Josh

--- js/application.js
+++ js/application.js
@@ -5,6 +5,7 @@ var map;
 var firstTime = true;
 var sliderTimer = '';
 $(document).ready(function() {
+	console.log('HELLO CODE');
 	geocoder = new google.maps.Geocoder(); //creats the ability for google to call this later
 	$('#input_field').submit(function(){ 
 		$('#loading').show();
